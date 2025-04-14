dbb9a1b9ef963e389d9b65665df641881f58a242
YairLozano <yclozanoquinteros@escuelasproa.edu.ar>
2025-04-14 12:11:51 -0300

    Add files via upload

---
+++ YairAbrilUno.py
@@ -0,0 +1,13 @@
+print ("registra y calcula el promedio de las notas de un estudiante")
+
+nota = []
+promedio = []
+
+for i in range (3):
+    notas = int(input("ingrese 3 numeros:"))
+    nota.append (notas)
+
+
+
+
+
