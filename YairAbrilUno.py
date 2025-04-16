7b22e5a74257670914260dd22ddc3609f2f56b72
YairLozano <yclozanoquinteros@escuelasproa.edu.ar>
2025-04-16 11:18:04 -0300

    Update YairAbrilDos.py

--- YairAbrilUno.py
+++ YairAbrilUno.py
@@ -1,13 +1,38 @@
-print ("registra y calcula el promedio de las notas de un estudiante")
+print ("Gestion de tareas pendientes")
 
-nota = []
-promedio = []
+def mostrar_menu():
+    print("gestor de tarea penientes")
+    print("escribir tarea")
+    print("ver todas las tareas")
+    print("Marcar una tarea como completada")
+    print("salir")
 
-for i in range (3):
-    notas = int(input("ingrese 3 numeros:"))
-    nota.append (notas)
+tareas = []
 
+while True:
+    mostrar_menu()
+    opcion = input("selecciona una opcion")
 
+if opcion == "1":
+    tarea = input("ingresa la descripcion de la tarea:")
+    tareas.append(tarea)
+    print("tarea escrita") 
 
+elif opcion == "2":
+    if not tareas:
+        print("no hay tareas pendientes")
+    else:
+        print("tareas pendientes")
+        for i,tarea in enumerate(tareas,1):
+            print("tarea")
 
-
+elif opcion == "3":
+    if not tareas:
+        print("no hay tareas para completar")
+    else:
+        for i,tarea in enumerate(tareas,1):
+            print("tarea")
+        num = input("ingresa el numero de la tarea completada")    
+               
+        if 1 <= num <= len(tareas):
+            print ("tare completada y eliminada")
